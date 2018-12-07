a9bf81031c3fc02dfa42726d4978d85e0127832d
Timur Galeev <timur_galeev@outlook.com>
2018-12-07 13:53:56 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# My OpenVPN config
+Change your config files
+1. Server config server.conf
+2. Change client config file ~/client-configs/base.conf
+Copy the mail form file into /etc/storage/ and chmod +x /etc/storage/mail.sh
+You need change mail.sh file the following ites FROM, AUTH, PASS, FROMNAME, TO
+## What you'll need
+- OpenVPN
+- SMTP server
+
+You can check the log by
+~~~
+status /var/log/openvpn/status.log # status log
+log /var/log/openvpn/serv.log # serv log
+
---
+++ mail.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+FROM="your-gmail-address"
+AUTH="your-gmail-username"
+PASS="your-gmail-password"
+FROMNAME="Your Router"
+TO="your-email-address"
+
+if [ -z "$1" ]; then
+    echo 'Say something.'
+    exit 1
+fi
+
+MSG=/tmp/e-mail.txt
+
+echo "From: $FROMNAME <$FROM>" > $MSG
+echo -e 'Subject: Message from router\n' >> $MSG
+echo -e $1 >> $MSG
+echo -e '\n---- \nYour friendly router' >> $MSG
+
+cat $MSG | sendmail -H 'exec openssl s_client -quiet -tls1_2 -connect smtp.gmail.com:465' \
+    -f"$FROM" -au"$AUTH" -ap"$PASS" $TO > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    echo 'Sent successfully.'
+else
+    echo 'Error while sending e-mail.'
+    exit 1
+fi
+rm $MSG
+
